3e124a62c21ef3db8d77193bdb4f933f4ae9f0ed
Naol Yizotw <naolyizotaw@gmail.com>
2025-02-22 13:21:35 +0300

    44

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,24 +1,20 @@
-"""
-URL configuration for LibraryProject project.
+# relationship_app/urls.py
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import list_books, LibraryDetailView
+from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("relationship_app/", include("relationship_app.urls")),
-
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
+    path('login/<int:pk>/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/<int:pk>/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
+    path('books/', list_books, name='list_books'),  
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'), 
+    path('books/add/', views.add_book, name="add_book/"),
+    path('books/edit/<int:book_id>/', views.edit_book, name="edit_book/"),
+    path('books/delete/<int:book_id>/', views.delete_book, name='delete_book'), 
 ]
