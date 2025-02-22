d48b168f1e24410eac69f097aced1598a303fc56
Naol Yizotw <naolyizotaw@gmail.com>
2025-02-22 13:36:45 +0300

    gggg

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,77 +1,26 @@
-from django.db import models
-from django.contrib.auth.models import User
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required, user_passes_test
 
-# Create your models here.
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
 
-# Create your models here.
-class Author(models.Model):
-    name = models.CharField(max_length=50)
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
 
-    def __str__(self):
-        return self.name
+def is_member(user):
+    return user.userprofile.role == 'Member'
 
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='author')
-    
-    def __str__(self):
-        return f"{self.title} by {self.author}"  
+@login_required
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin_view.html')
 
-#Extending Book Model with Custom Permissions
-    class Meta(models.Model):
-        Permissions_Choices =(
-            ('can_add_book', 'can_add_book'),
-            ('can_change_book', 'can_change_book'),
-            ('can_delete_book', 'can_delete_book'),
+@login_required
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'librarian_view.html')
 
-        )
-    permissions = models.CharField(max_length=50,  choices='Permissions_Choices')
-    meta = models.TextField()
-    
-    def __str__(self):
-        return f'{self.user.username} - {self.permissions}'
-
-class Library(models.Model):
-    name = models.CharField(max_length=200)
-    books = models.ManyToManyField(Book, related_name='libraries')
-
-    def __str__(self):
-        return self.name   
-
-class Librarian(models.Model):
-    name = models.CharField(max_length=200)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarians')
-
-    def __str__(self):
-        return self.name
-    
-#Extending User Model with a UserProfile
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    
-    Role_Choices =(
-        ('Admin','Admin'),
-        ('Librarian', 'Librarian'),
-        ('Member', 'Member'),
-    
-    )
-
-role = models.CharField(max_length=50,  choices='Role_Choices')
-userprofile = models.TextField()
-
-def __str__(self):
-    return f'{self.user.username} - {self.role}'
-
-#Signal to automatically create a UserProfile when a new User is created
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.userprofile.save()
+@login_required
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'member_view.html')
