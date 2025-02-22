3ed73172b74c9a428c92ceaffcec75b5e08849b6
Naol Yizotw <naolyizotaw@gmail.com>
2025-02-22 14:13:39 +0300

    l

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -17,8 +17,6 @@ urlpatterns = [
     path('books/add/', views.add_book, name="add_book/"),
     path('books/edit/<int:book_id>/', views.edit_book, name="edit_book/"),
     path('books/delete/<int:book_id>/', views.delete_book, name='delete_book'), 
-    path('admin-view/', admin_view, name='admin_view'),
-    path('librarian-view/', librarian_view, name='librarian_view'),
-    path('member-view/', member_view, name='member_view'),
+    
     
 ]
